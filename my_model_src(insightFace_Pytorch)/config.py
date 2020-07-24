d7bc39c9c96538fd496945800bd91849ac1c440f
shiney5213 <shiney5213@gmail.com>
2020-07-24 09:49:57 +0900

    Update config.py

--- my_model_src(insightFace_Pytorch)/config.py
+++ my_model_src(insightFace_Pytorch)/config.py
@@ -11,6 +11,7 @@ def get_config(training = True):
     conf.model_path = conf.work_path/'models'
     conf.log_path = conf.work_path/'log'
     conf.save_path = conf.work_path/'save'
+    conf.history_path = conf.work_path/'history'
     conf.input_size = [112,112]
     conf.embedding_size = 512
     conf.use_mobilfacenet = False
@@ -30,9 +31,12 @@ def get_config(training = True):
     conf.vgg_folder = conf.data_path/'faces_vgg_112x112'
     conf.ms1m_folder = conf.data_path/'faces_ms1m_112x112'
     conf.emore_folder = conf.data_path/'faces_emore'
-    # conf.batch_size = 100 # irse net depth 50 
+    conf.emorekface_folder = conf.data_path/'emore_kface'
+
+
+    conf.batch_size = 100 # irse net depth 50 
 #   conf.batch_size = 200 # mobilefacenet
-    conf.batch_size = 32 # mobilefacenet
+    # conf.batch_size = 32 # small_vgg
 
 #--------------------Training Config ------------------------    
     if training:        
